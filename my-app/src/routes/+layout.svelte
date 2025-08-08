6d00e87c590fd23784cada90a8c04b80aa393b1b
タピオカ狐 <magneticchen@gmail.com>
2025-08-08 01:27:38 -0400

    Basic layouts.

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -8,4 +8,28 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<style>
+	#main-nav {
+		background-color: black;
+		color: white;
+		padding: 4px 8px;
+		font-family: 'Courier New', Courier, monospace;
+	}
+	
+	#main-nav > a {
+		color: white;
+		text-decoration: none;
+		margin: 0 0px;
+	}
+</style>
+
+<nav id="main-nav">
+	<a href="/">TapiocaFox</a>
+	<!-- <a href="https://github.com/TapiocaFox/Daijishou">Daijishō</a> -->
+	<a href="/css">[Site CSS]</a>
+	<a href="/glsl">[GLSL]</a>
+	<a href="/artworks">[Artworks]</a>
+	<a href="https://github.com/TapiocaFox">[GitHub]</a>
+</nav>
+
 {@render children?.()}
---
+++ my-app/src/routes/css/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Header 1</h1>
+<h2>Header 2</h2>
+<h3>Header 3</h3>
+<h4>Header 4</h4>
+<h5>Header 5</h5>
+<p>I was a grotesque amalgam of timidity and arrogance, alternating between long, awkward silences and blazing fits of rambunctiousness.</p>
+<hr class="dashed">
+<hr class="dotted">
+<hr class="solid">
+<hr class="rounded">
