b1bc9044720ec01d6048b4de30e2f3202492bd1d
タピオカ狐 <magneticchen@gmail.com>
2025-08-15 05:21:47 -0400

    Added more content.

--- src/routes/artworks/portraits/+page.svelte
+++ src/routes/artworks/portraits/+page.svelte
@@ -19,9 +19,9 @@
 <p>Pretty people and some significant individuals in my life. (Does not include every one of them due to many reasons.)</p>
 <img class="portrait" alt="Snake" src={snake}/>
 <p class="annotation">A woman with a snake wrapped around her neck. There was a reference picture on Pinterest, but I've lost it.</p>
-<img class="portrait" alt="Lin Yi-Bing" src={lin_yi_bing}/>
-<p class="annotation">Prof. Lin Yi-Bing (林一平), he was the vice president of my undergrade university NCTU. He is a very talented person, who also enjoys sketching and other forms of arts. He was very insisting in his own belief; having a lots of pride, but also a very open-minded person. I was invited into his lab, and he wrote me a recommandation letter that get me into NYU.</p>
 <img class="portrait" alt="jyneda" src={jyneda}/>
-<p class="annotation">Prof. Jyneda (Yun-Wei Lin), he was a very talkative and engaging person. He introduced me many concepts like micro-service and other internet paradigms. He was a very pragmatic, humble, and patient person. He also wrote me a recommandation letter that get me into NYU.</p>
+<p class="annotation">Prof. Jyneda (Yun-Wei Lin), he was a very informed, talkative and engaging person. He introduced me many concepts like micro-service and other internet paradigms. He was a very pragmatic, humble, and patient person. He also me a recommandation letter that get me into NYU.</p>
+<img class="portrait" alt="Lin Yi-Bing" src={lin_yi_bing}/>
+<p class="annotation">Prof. Lin Yi-Bing (林一平), he was the vice president of my undergrade university NCTU. He is a very insightful person, who also enjoys sketching and other forms of arts. He was very insisting in his own belief; having a lots of pride, but also a very open-minded person. I was invited into his lab, and he wrote me a recommandation letter that get me into NYU.</p>
 <img class="portrait" alt="stubbs" src={stubbs}/>
 <p class="annotation">Mr. Stubbs, the founder and the manager of the largest retro gamming community "Retro Handhelds". We chatted on Discord, he is very good at communicating and connecting people and developers from different backgrounds. He has a very sweet family and seems to live in the woods? He ALSO wrote me a recommandation letter.</p>
--- src/routes/artworks/sketchings/+page.svelte
+++ src/routes/artworks/sketchings/+page.svelte
@@ -51,14 +51,14 @@
 <img class="sketching" alt="Fox girl" src={fox_girl}/>
 <p class="annotation">I am not into furry. But one day I woke up with this idea.</p>
 <img class="sketching" alt="The vampire" src={vampire}/>
-<p class="annotation">It is techniquely not a sketch, but I don't know where to put it.</p>
+<p class="annotation">It is techniquely not a sketch, but I don't know where to put it. I feel high while drawing this.</p>
 <img class="sketching" alt="Demon in suit" src={demon_in_suit}/>
-<p class="annotation">Something evil but also civilised.</p>
+<p class="annotation">Try draw something evil but also civilised thing.</p>
 <img class="sketching" alt="Birdes" src={birdies}/>
-<p class="annotation">Some cute birdies :3.</p>
+<p class="annotation">Some cute birdies chatting :3.</p>
 <img class="sketching" alt="Ping Pong" src={ping_pong}/>
-<p class="annotation">I used to be the president of table tennis club. So it means something to me.</p>
+<p class="annotation">I used to be the president of table tennis club. It was a huge part in my undergrad life. So it means something to me.</p>
 <img class="sketching_in_row" alt="Miku" src={miku}/><img class="sketching_in_row" alt="Miku Reference" src={miku_reference}/>
-<p class="annotation">Hatsune Miku.</p>
+<p class="annotation">Hatsune Miku. Got this sticker from Tokyo Japan. I like how it looks sharp and detailed.</p>
 <img class="sketching_in_row" alt="Airplane 1" src={airplane_1}/><img class="sketching_in_row" alt="Airplane 2" src={airplane_2}/>
-<p class="annotation">Fighter jets. I definitely know nothing about aero dynamic. But I like the precisional feeling of the fighter jet. I am amazed to think about the components of a jet require constant renewal and maintenance until it is no longer the same one. One exterme example is the F-16V model in Taiwan. Despite it's old inheriting design, and relatively long service period, the constant update of avionics make the F-16s still very competitive with mordern combat. While keeping the cost efficiency reasonable. (Taiwanese defense budget is relatively small compare to its hostile neighbor. And the American worries about Taiwan leaking intel to China with newer fighter models, due to low linguistic and cultural barrier.)</p>
+<p class="annotation">Fighter jets. I definitely know nothing about aero dynamic. But I like the precisional feeling of the fighter jet. I am amazed to think about the components of a jet require constant renewal and maintenance until it is no longer the same one. One exterme example is the F-16V model in Taiwan. Despite it's old inheriting design, and relatively long service period, the constant update of avionics make the F-16s still very competitive in the mordern combat. While keeping the cost efficiency reasonable. (Taiwanese defense budget is relatively small compare to its hostile neighbor. And the American worries about Taiwan leaking intel to China if possesses newer fighter models, due to low linguistic and cultural barrier in between.)</p>
