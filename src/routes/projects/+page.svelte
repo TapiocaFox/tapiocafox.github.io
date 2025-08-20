b6b2dadcd8f59b4385519e6a4e158f0f5f48b227
タピオカ狐 <magneticchen@gmail.com>
2025-08-20 03:18:53 -0400

    Built and published by script.

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Projects 💻</h1>
 <p class="annotation">(The website is still very barebone; Please visit my <a href="https://github.com/TapiocaFox" target="_blank">GitHub</a> instead.)</p>
 <h2><a href="https://github.com/TapiocaFox/Daijishou" target="_blank">Daijishō</a></h2>
-<p class="compact">Daijishō is a android launcher that let you manage your retro games libraries. This repo is for assets and served as a main page. It has 1M+ users and 1.4k stars on GitHub.</p>
+<p class="compact">Daijishō is a android launcher that let you manage your retro games libraries. This repo is for assets and served as a main page. It has 1M+ downloads and 1.4k stars on GitHub.</p>
 <h2><a href="/glsl">GLSL</a></h2>
 <p class="compact">Personal practice of GLSL. Shaders use GLSL (OpenGL Shading Language), a special OpenGL Shading Language with syntax similar to C. <a href="https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_on_the_web/GLSL_Shaders" target="_blank">more…</a></p>
