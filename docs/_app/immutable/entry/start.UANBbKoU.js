4edf7d42bf7cad0ad9a2c1731b5de6f531c928ff
タピオカ狐 <magneticchen@gmail.com>
2025-08-31 01:10:41 -0400

    Built and published by script.

---
+++ docs/_app/immutable/entry/start.UANBbKoU.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CelCW5JR.js";export{o as load_css,r as start};
--- src/routes/media/+page.svelte
+++ src/routes/media/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 <h1>Media</h1>
 <ChipsWithUrlHash 
-  names={['Music', 'Videos']} 
+  names={['Karaoke', 'Retina']} 
   values={['music', 'videos', 'photos', 'other']}
   selected_value={selected_category}
   callback={(value: any) => {
@@ -15,6 +15,7 @@
 
 
 {#if selected_category=="music"}
+<!-- <p class="bold-annotation">Karaoke but you don't have to sing.</p> -->
 <iframe title="Apple Music playlist" allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/playlist/tapiocafoxs-selection/pl.u-EdAVR7Gu5Wq7yq"></iframe>
 {/if}
 
