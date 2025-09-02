0ef0684a40b0a4725c9e928fb1db3fabb18168f7
タピオカ狐 <magneticchen@gmail.com>
2025-09-02 03:16:39 -0400

    Built and published by script.

---
+++ docs/_app/immutable/nodes/2.BkGGzEpJ.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import{f as s,a as t,c as p,n as e,s as m,r as d}from"../chunks/Bqm8lQsz.js";import{s as v}from"../chunks/Cn_BCs_p.js";import{E as c}from"../chunks/lEe-b8Ov.js";var l=s('<div class="align-with-nav"><!> <!></div>');function u(r,i){var o=l(),a=p(o);v(a,()=>i.children??e);var n=m(a,2);c(n),d(o),t(r,o)}export{u as component};
---
+++ src/lib/components/EndingDecorationV5.svelte
@@ -0,0 +1,26 @@
+<script>
+    import icon from '$lib/assets/icon_dark.png';
+    import tapiocafox from '$lib/assets/tapiocafox_small.png';
+</script>
+<div 
+    style:display="flex"
+    style:color="black"
+    style:margin-block-start="1rem"
+    style:margin-block-end="1rem"
+    style:gap="8px">
+    <div style:width="auto">
+        <img alt="Icon" style:height="48px" style:width="48px" src={icon}/>
+    </div>
+    <div 
+        style:flex-grow="1"
+        style:opacity="1"
+        style:display="flex"
+        style:align-items="center">
+        <div>
+            <img alt="TapiocaFox" style:height="22px" src={tapiocafox}/>
+            <p style:margin="0"
+            style:font-family="'Courier New', Courier, monospace"
+            style:font-size="xx-small">©2025 TapiocaFox. End of the page.</p>
+        </div>
+    </div>
+</div>
