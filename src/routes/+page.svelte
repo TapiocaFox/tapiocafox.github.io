206e1b9b1c0dfa5d4deb3c9511301060a88f20bd
タピオカ狐 <magneticchen@gmail.com>
2025-09-04 21:26:12 -0400

    Built and published by script.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,8 +56,8 @@
         </div>
     </div>
     <hr class="dashed fade-in load-delay">
-    <p class="compact fade-in load-delay" >Hi. This is the website of me sharing personal works and demos. Including digital <a href="/artworks">artworks</a>, previous <a href="/projects">projects</a>, and also some ongoing ones.</p>
-    <p class="compact fade-in load-delay" >I’m the creator of the retro game management software <a href="https://github.com/TapiocaFox/Daijishou" target="_blank">Daijishō</a> (~1M accumalated users). I built frameworks that can interact efficiently and easy to understand.</p>
+    <p class="compact fade-in load-delay" >Hi. This is the website of me sharing personal works and demos. Including digital <a href="/artworks">artworks</a>, selected <a href="/projects">projects</a>, and also some ongoing ones.</p>
+    <p class="compact fade-in load-delay" >I’m the creator of the retro game management software <a href="https://github.com/TapiocaFox/Daijishou" target="_blank">Daijishō</a> (~1M accumalated users). I enjoy building frameworks that can interact efficiently and easy to understand.</p>
     <!-- <h3>The music I like recently</h3> -->
     <!-- <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/us/album/2000-mixed/1829001427?i=1829001428"></iframe> -->
     <!-- <p class="annotation">The music I vibe with recently.</p> -->
