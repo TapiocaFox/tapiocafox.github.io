9ef7407ffbe1260804a0429d76c1b7bc0219305c
タピオカ狐 <magneticchen@gmail.com>
2025-09-11 00:32:09 -0400

    Built and published by script.

---
+++ docs/_app/immutable/entry/start.BACv--Lv.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/D0FQ8K1_.js";export{o as load_css,r as start};
--- src/routes/glsl/+page.svelte
+++ src/routes/glsl/+page.svelte
@@ -5,10 +5,11 @@
     import shader_toy_demo from '$lib/assets/glsl_shaders/shader_toy_demo.frag?raw';
     import sin from '$lib/assets/glsl_shaders/sin.frag?raw';
     import abstract from '$lib/assets/glsl_shaders/abstract.frag?raw';
+    import edit_icon from '$lib/assets/icons/edit.svg';
     
 </script>
 <HeaderWithBackButton text="GLSL shader"/>
-<p class="annotation">Personal practice of GLSL. Try it in <a href="/glsl/editor">editor</a>.</p>
+<p class="annotation">Personal practice of GLSL. Try it in <img class="inline-icon" alt="Edit" src={edit_icon}/><a href="/glsl/editor">editor</a>.</p>
 <GlslCanvas/>
 <!-- <GlslCanvas fragment_shader={alter_green_red_frag}/> -->
 <!-- <GlslCanvas fragment_shader={shader_toy_demo}/> -->
